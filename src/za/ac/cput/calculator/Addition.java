f6aff466cdf20ef6465228892a5cca33799c71ad
Sinovuyo <sinovuyomlanjeni137@gmail.com>
2021-03-28 09:46:14 +0200

    Adding a .gitignore fole

---
+++ src/za/ac/cput/calculator/Addition.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package za.ac.cput.calculator;
+
+/**
+ *
+ * @author Azania
+ */
+public class Addition {
+    public static int sum(final int a, final int b){
+        return a + b;
+                }
+     }
+
